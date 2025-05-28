77b09f0e1167c835c0d5f0fa649512733c2eddf4
Sirius912 <165539235+Sirius912@users.noreply.github.com>
2025-05-28 19:15:53 +0900

    refactor: change gesture index

--- main.py
+++ main.py
@@ -4,34 +4,32 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-# 1. 손 인식 개수 및 제스처 정의
-max_num_hands = 1 # 최대 몇 개의 손을 인식할 건지 정의
+max_num_hands = 1
 my_gesture = {
     0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-    6: 'k-heart', 7: 'pig', 8: 'racist', 9: 'heart', 11: 'fy'
+    6: 'k-heart', 7: 'fig', 8: 'racist', 9: 'heart', 10: 'fy'
 }
 
-# 2. Mediapipe hands model (모델 1 - 손 감지 모델)
 mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils # 손 위에 그림을 그릴 수 있는 메소드
-# hands 손가락 인식모듈의 작동 option을 지정한다. 
+# mp_drawing = mp.solutions.drawing_utils # 손 위에 그림을 그릴 수 있는 메소드
+
 hands = mp_hands.Hands(
-    max_num_hands=max_num_hands, # 인식할 손모양의 갯수, 생략하면 2가 지정된다.
-    min_detection_confidence=0.5, # 성공적인 것으로 간주되는 최소 신뢰도 값. 0.0 ~1.0사이로서 기본값은 0.5이다.
-    min_tracking_confidence=0.5) # 손 랜드마크가 성공적으로 추적된 것으로 간주되는 최소 신뢰도 값. 0.0 ~1.0 사이로서 기본값은 0.5이다. 이 값을 높이면 시간이 더 소요되지만 좀 더 정확한 작동이 보장된다. 
+    max_num_hands=max_num_hands,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
  
 # 3. Gesture recognition model (모델 2 - 제스처 인식 모델)
 file = np.genfromtxt('gesture_train.csv', delimiter=',') # data file(손가락의 각도들과 label 값 저장 파일)
 angle = file[:,:-1].astype(np.float32)
 label = file[:, -1].astype(np.float32)
-knn = cv2.ml.KNearest_create() # K-Nearest Neighbors 알고리즘(K-최근접 알고리즘)
-knn.train(angle, cv2.ml.ROW_SAMPLE, label) # 학습 시키기
+knn = cv2.ml.KNearest_create()
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)
 
-# OpenCV로 웹캠을 읽어 입력데이터 소스로 지정한다. 
-cap = cv2.VideoCapture(0) # USB 캠을 활용하고 싶다면 0 대신 1
+
+cap = cv2.VideoCapture(1) # USB 캠을 활용하고 싶다면 0 대신 1
 
 while cap.isOpened():
-    ret, img = cap.read() 	# 한 프레임씩 이미지 읽어옴
+    ret, img = cap.read()
     if not ret:
         continue
     # 전처리 (opencv: BGR, mediapipe: RGB)
@@ -55,20 +53,19 @@ while cap.isOpened():
             # Normalize v
             v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]  # 길이로 나눠줌 (크키 1짜리 vector 나오게 됨(unit vector))
 
-            # Get angle using arcos of dot product (15개의 각도 구하기)
+            # Get angle using arcos of dot product
             angle = np.arccos(np.einsum('nt,nt->n',
                 v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
-                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
             
             # Convert radian to degree
-            angle = np.degrees(angle) # angle이 radian 값으로 나오기 때문에 degree 값으로 바꿔줌
+            angle = np.degrees(angle)
 
             # Inference gesture
             data = np.array([angle], dtype=np.float32)
             ret, results, neighbours, dist = knn.findNearest(data, 3)
             idx = int(results[0][0])
 
-            #---------------------------------------------------------------------------------
             # Draw gesture result
             if idx in my_gesture.keys():
                 org = (int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0]))
@@ -80,12 +77,10 @@ while cap.isOpened():
                     'org': org
                 })
             
-                # 추가: 제스처가 7(pig) 또는 11(fy)일 때만 이미지 왜곡 효과 적용
-                if idx == 7 or idx == 11:
+                if idx == 7 or idx == 8 or idx == 10 :
                     x1, y1 = tuple((joint.min(axis=0)[:2] * [img.shape[1], img.shape[0]] * 0.95).astype(int))
                     x2, y2 = tuple((joint.max(axis=0)[:2] * [img.shape[1], img.shape[0]] * 1.05).astype(int))
             
-                    # x1, y1, x2, y2 값이 이미지 범위를 벗어나지 않도록 제한
                     x1 = max(x1, 0)
                     y1 = max(y1, 0)
                     x2 = min(x2, img.shape[1])
@@ -100,7 +95,7 @@ while cap.isOpened():
                         img[y1:y2, x1:x2] = fy_img
 
 
-            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+            # mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
 
 
     cv2.imshow('Game', img)
